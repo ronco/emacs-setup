aad860fda8a262a9db1f30547595ad673a94f4e4
Ron <ronco@costite.com>
2015-04-28 14:07:25 -0600

    some git-gutter+ keybindings

--- emacs-setup.el
+++ emacs-setup.el
@@ -342,6 +342,21 @@
 (if (file-exists-p "/opt/boxen/homebrew/bin/emacsclient")
     (setq magit-emacsclient-executable "/opt/boxen/homebrew/bin/emacsclient"))
 
+;; git gutter +
+(global-set-key (kbd "C-x G") 'global-git-gutter+-mode) ; Turn on/off globally
+(eval-after-load 'git-gutter+
+  '(progn
+     ;;; Jump between hunks
+     (define-key git-gutter+-mode-map (kbd "C-x n") 'git-gutter+-next-hunk)
+     (define-key git-gutter+-mode-map (kbd "C-x p") 'git-gutter+-previous-hunk)
+
+     ;;; Act on hunks
+     (define-key git-gutter+-mode-map (kbd "C-x v =") 'git-gutter+-show-hunk)
+     (define-key git-gutter+-mode-map (kbd "C-x r") 'git-gutter+-revert-hunks)
+     ;; Stage hunk at point.
+     ;; If region is active, stage all hunk lines within the region.
+     (define-key git-gutter+-mode-map (kbd "C-x t") 'git-gutter+-stage-hunks)
+))
 
 ;; GLOBAL BINDINGS
 
