b0ec7b6d3dc0a6c1b494b7b824f977ae91911d9c
Ron <ronco@costite.com>
2015-04-28 08:27:56 -0600

    js & coffee snippets

---
+++ snippets/coffee-mode/.yas-compiled-snippets.el
@@ -0,0 +1,15 @@
+;;; Compiled snippets and support files for `coffee-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'coffee-mode
+                     '(("ib-anal" "  analytics: Em.inject.service('google-analytics')" "inject analytics" nil nil nil nil "direct-keybinding" nil)
+                       ("ib-data" "\\`import ibottaData from 'ibotta-web/utils/ibotta-data'\\`" "import ibotta data" nil nil nil nil "direct-keybinding" nil)
+                       ("ib-element" "\\`import ModelElementIdMixin from 'ibotta-web/mixins/model-element-id'\\`\n" "model element id" nil nil nil nil "direct-keybinding" nil)
+                       ("ib-env" "\\`import ENV from 'ibotta-web/config/environment'`" "import environment" nil nil nil nil "direct-keybinding" nil)
+                       ("ib-log" "\\`import logger from 'ibotta-web/utils/logger'\\`\n" "import logger" nil nil nil nil "direct-keybinding" nil)
+                       ("ib-session" "  session: Em.inject.service 'session'" "inject session" nil nil nil nil "direct-keybinding" nil)
+                       ("ib-static" "\\`import staticConstants from 'ibotta-web/utils/static-constants'\\`\n" "import static constants" nil nil nil nil nil nil)
+                       ("ib-utils" "\\`import ibottaUtilities from 'ibotta-web/utils/ibotta-utilities'\\`" "import utilities" nil nil nil nil "direct-keybinding" nil)))
+
+
+;;; Do not edit! File generated at Fri Apr 17 12:07:43 2015
