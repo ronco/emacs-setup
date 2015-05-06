0579c5e85f161a64270a70ccd642bfd1cc993006
ronco <ronco@costite.com>
2015-05-06 12:29:51 -0600

    yasnippet loading cleanup

--- emacs-setup.el
+++ emacs-setup.el
@@ -231,23 +231,6 @@
 
 ; code templates
 (require 'yasnippet)
-;; (append yas-snippet-dirs (list
-;;                           yas-installed-snippets-dir
-;;                           "/Users/ronco/github/ember-yasnippets.el/snippets"
-;;                           (concat ronco-es/dir "/ib-snippets")
-;;                           (concat ronco-es/dir "/snippets")
-;;                           (concat ronco-es/dir "/yasnippet-snippets")
-;;                           ))
-
-;; (eval-after-load "yasnippet"
-;;   (lambda ()
-;;     (add-to-list 'yas-snippet-dirs (concat ronco-es/dir "/ib-snippets"))
-;;     (add-to-list 'yas-snippet-dirs (concat ronco-es/dir "/snippets"))
-;;     (add-to-list 'yas-snippet-dirs (concat ronco-es/dir "/yasnippet-snippets"))
-;;     (yas-load-directory (concat ronco-es/dir "/ib-snippets"))
-;;     (yas-load-directory (concat ronco-es/dir "/snippets"))
-;;     (yas-load-directory (concat ronco-es/dir "/yasnippet-snippets"))
-;;     ))
 (eval-after-load "yasnippet"
   (lambda ()
     (dolist (yadir (list
