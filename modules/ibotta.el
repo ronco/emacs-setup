eeb387a6c72d48596f75577b9248ac6be402057f
ronco <ronco@costite.com>
2015-05-12 10:38:32 -0600

    ibotta configs

--- modules/ibotta.el
+++ modules/ibotta.el
@@ -5,3 +5,20 @@
   )
 (add-hook 'coffee-mode-hook 'ibotta-coffee-mode-hook)
 (customize-set-variable 'coffee-command "/Users/ronco/bin/black-coffee.sh")
+
+(defun dev-db ()
+  (interactive)
+  (ronco-sql-connect 'mysql 'dev))
+
+(defun staging-db ()
+  (interactive)
+  (ronco-sql-connect 'mysql 'staging))
+
+(defun reporting-db ()
+  (interactive)
+  (ronco-sql-connect 'mysql 'reporting))
+
+(defun ronco-sql-connect (product connection)
+  (require 'ib-sql "ib-sql.el.gpg")
+  (setq sql-product product)
+  (sql-connect connection))
