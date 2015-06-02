5d077e82ad915304c98369cd2a188912df0d4b07
ronco <ronco@costite.com>
2015-06-01 19:55:01 -0600

    fix js2-init-hook

--- modules/ember.el
+++ modules/ember.el
@@ -4,8 +4,8 @@
 (setq create-lockfiles nil)
 
 ;; add mocha test specific globals
-(add-hook 'js2-mode-hook
-      (lambda ()
-        (when (string-match-p "tests" (buffer-file-name))
+(add-hook 'js2-init-hook
+          (lambda ()
+        (when (string-match-p "test" (buffer-file-name))
           (setq js2-additional-externs '("document" "window" "location" "setTimeout" "$" "-Promise" "define" "console" "DS" "sandbox" "describe" "beforeEach" "afterEach" "before" "after"))
           )))
