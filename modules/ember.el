5e6ed846b7580bf6bc0f0fc9089dd2a6f3fc5a78
ronco <ronco@costite.com>
2016-01-06 14:26:59 -0700

    jscs helper method

--- modules/ember.el
+++ modules/ember.el
@@ -2,3 +2,30 @@
 
 ;; disable lock files because broccoli
 (setq create-lockfiles nil)
+
+;; cleanup functions
+
+
+(defun wrap-text (b e pretext posttext)
+  "simple wrapper"
+  (interactive "r\nMEnter text to prefix with: \nMEnter text to suffix with: ")
+  (save-restriction
+    (narrow-to-region b e)
+    (goto-char (point-min))
+    (insert pretext)
+    (insert "\n")
+    (goto-char (point-max))
+    (insert posttext)
+    )
+  )
+
+(defun wrap-jscs-disble (b e rule)
+  "wrap region in a disabled jscs rule"
+  (interactive "r\nMEnter the rule to disable: ")
+  (let (
+        (pretext (concat "// jscs: disable " rule))
+        (posttext (concat "// jscs: enable " rule "\n"))
+        )
+    (wrap-text b e pretext posttext)
+    )
+  )
