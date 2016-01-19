ccda105199051374ec6b03c0b303824c8f502f31
ronco <ronco@costite.com>
2016-01-19 13:06:44 -0700

    don't fold { } in ruby

--- emacs-setup.el
+++ emacs-setup.el
@@ -534,8 +534,8 @@
 (eval-after-load "hideshow"
   '(add-to-list 'hs-special-modes-alist
                  `(ruby-mode
-                   ,(rx (or "def" "class" "module" "do" "{" "[")) ; Block start
-                   ,(rx (or "}" "]" "end"))                  ; Block end
+                   ,(rx (or "def" "class" "module" "do")) ; Block start
+                   ,(rx (or "end"))                  ; Block end
                    ,(rx (or "#" "=begin"))                   ; Comment start
                    ruby-forward-sexp nil)))
 
