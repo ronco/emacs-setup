060125e42841652245158d984d8f0864b98e110c
ronco <ronco@costite.com>
2017-01-11 10:47:56 -0700

    hardcode python path

--- bin/gcal-download.sh
+++ bin/gcal-download.sh
@@ -6,8 +6,7 @@ ICS2ORG=/Users/ronco/github/ical2org.py/ical2org.py
 ICSFILE=/tmp/cal.ics
 ORGFILE="/Users/ronco/Dropbox (Ibotta)/ron-notes/org/calendar.org"
 URL=$1
-
 # no customization needed below
 
 $WGET -O $ICSFILE $URL
-$ICS2ORG $ICSFILE > "$ORGFILE"
+/opt/boxen/homebrew/bin/python $ICS2ORG $ICSFILE > "$ORGFILE"
