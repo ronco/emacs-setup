c132ea8a60347e9db89edd4b904e995cedce49ae
Ron White <ron@ibotta.com>
2017-07-12 16:49:36 -0600

    change cal location

--- bin/gcal-download.sh
+++ bin/gcal-download.sh
@@ -4,7 +4,7 @@
 WGET=/opt/boxen/homebrew/bin/wget
 ICS2ORG=/Users/ronco/github/ical2org.py/ical2org.py
 ICSFILE=/tmp/cal.ics
-ORGFILE="/Users/ronco/Dropbox (Ibotta)/ron-notes/org/calendar.org"
+ORGFILE="/Users/ronco/Box Sync/ron-notes/org/calendar.org"
 URL=$1
 # no customization needed below
 
